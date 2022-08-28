c510b60364e2f1637508b4bbaaf9706fbff57731
RayChromium <raychromium@outlook.com>
2022-08-28 22:58:27 +0800

    data types (18:35)

---
+++ javascript/js-crashcourse/main.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+
+// let, const
+// always use const unless you know you need to do reasign
+
+// Data Types:
+// String, Number,  Boolean,  null, undefined, Symbol
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; //undefined
+
+console.log(typeof(name)); // or console.log(typeof name)  // string
+console.log(typeof(age));       // number
+console.log(typeof(rating));    // number
+console.log(typeof(isCool));    // boolean
+console.log(typeof(x));         // object
+console.log(typeof(y));         // undefined
+console.log(typeof(z));         // undefined
