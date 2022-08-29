e748d5d13599ac2084ada119f978caaa7f2ab638
RayChromium <raychromium@outlook.com>
2022-08-29 23:12:33 +0800

    array functional programming intro done

--- javascript/js-crashcourse/main.js
+++ javascript/js-crashcourse/main.js
@@ -7,16 +7,139 @@ console.log('Hello World');
 // String, Number,  Boolean,  null, undefined, Symbol
 const name = 'John';
 const age = 30;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z; //undefined
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z; //undefined
 
-console.log(typeof(name)); // or console.log(typeof name)  // string
-console.log(typeof(age));       // number
-console.log(typeof(rating));    // number
-console.log(typeof(isCool));    // boolean
-console.log(typeof(x));         // object
-console.log(typeof(y));         // undefined
-console.log(typeof(z));         // undefined
+// console.log(typeof(name)); // or console.log(typeof name)  // string
+// console.log(typeof(age));       // number
+// console.log(typeof(rating));    // number
+// console.log(typeof(isCool));    // boolean
+// console.log(typeof(x));         // object
+// console.log(typeof(y));         // undefined
+// console.log(typeof(z));         // undefined
+
+// Strings
+// Concatenation
+console.log('My name is ' + name + ' and my age is ' + age);
+// Template strings
+console.log(`My name is ${name} and my age is ${age}`);
+const hello = `My name is ${name} and my age is ${age}`;
+console.log(hello);
+
+const s = 'Hello World!';
+const s1 = 'technology, it, computers, code';
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0,5).toUpperCase());
+console.log(s.split(''));
+console.log(s1.split(', '));
+
+// Arrays
+// array constructor
+const numbers = new Array(1, 2, 3, 4, 5);
+console.log(numbers);
+// common way
+const fruits = ['apples', 'oranges', 'pears', 10, true];
+console.log(fruits);
+console.log(fruits[1]); // oranges
+fruits[3] = 'grapes';
+console.log(fruits);
+fruits.push('mangos');
+console.log(fruits);
+fruits.unshift('strawberries');
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(s1));
+console.log(fruits.indexOf('oranges'));
+
+// object literals
+const person = {
+    firstName:'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies:['music','movies','sports'],
+    address:{
+        street:'50 main street',
+        city:'Boston',
+        state:'MA'
+    }
+}
+console.log(person);
+console.log(person.firstName);
+console.log(person.lastName, person.age);
+// de-construcitng
+const { firstName, lastName, address:{city} } = person; // pulling things out from object
+console.log(firstName, lastName, city);
+person.email = 'john@gmail.com';
+console.log(person);
+
+const todos = [
+   {
+      id:1,
+      text : 'Take out the trash',
+      isComplete:true
+   },
+   {
+      id:2,
+      text : 'Meet the boss',
+      isComplete:true
+   },
+   {
+      id:3,
+      text : 'Dentist appt',
+      isComplete:false 
+   }
+
+];
+
+console.log(todos);
+
+// convert to JSON
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
+
+// for loop
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// while loop
+// let i = 0;
+// while(i < 10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+for( let todo of todos )
+{
+    console.log(todo.text);
+}
+
+// forEach, map, filter
+todos.forEach(function(todo){
+    console.log(todo.text);
+});
+
+const todoText = todos.map(function(todo){
+    return todo.text;
+});
+console.log(todoText);
+
+const todoCompleted = todos.filter(function(todo){
+    return todo.isComplete === true;
+});
+console.log(todoCompleted);
+
+const todoCompletedText = todos.filter(function(todo){
+    return todo.isComplete === true;
+}).map(function(todo){
+    return todo.text;
+});
+console.log(todoCompletedText);
