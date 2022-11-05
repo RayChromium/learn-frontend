1f22c72753c40091c87da0342415d2c3c665f87c
RayChromium <raychromium@outlook.com>
2022-11-05 14:55:37 +0800

    properties of documnet

---
+++ javascript/js_DOM_crashcourse/dom.js
@@ -0,0 +1,14 @@
+// examine the document object
+// console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+// document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+// document.all[10].textContent = 'fuck'; // don't do this
+console.log(document.forms);
+console.log(document.links);
