c840815a8404bf8d6addc188543befc1dd54d616
RayChromium <raychromium@outlook.com>
2022-11-05 21:33:27 +0800

    getElementByTagName

--- javascript/js_DOM_crashcourse/dom.js
+++ javascript/js_DOM_crashcourse/dom.js
@@ -35,3 +35,16 @@
 // {
 //     items[i].style.backgroundColor = '#f4f4f4';
 // }
+
+// // getElementsByTagName
+// const li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello too';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// for(let i = 0; i < li.length; ++i)
+// {
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
