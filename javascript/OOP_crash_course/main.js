d0088df15bb9e0431614bac3df336dea838562f2
RayChromium <raychromium@outlook.com>
2022-11-28 22:14:12 +0800

    obj literals and ctor (outdated)

--- javascript/OOP_crash_course/main.js
+++ javascript/OOP_crash_course/main.js
@@ -9,24 +9,38 @@
 // window.alert(1);
 
 // object literals:
-const book1 = {
-    title: 'book 1',
-    author : 'John Doe',
-    year : '2013',
-    getSumary : function(){ return `${this.title}, written by ${this.author} in ${this.year}`;}
-};
+// const book1 = {
+//     title: 'book 1',
+//     author : 'John Doe',
+//     year : '2013',
+//     getSumary : function(){ return `${this.title}, written by ${this.author} in ${this.year}`;}
+// };
 
-const book2 = {
-    title: 'book 2',
-    author : 'Jane Doe',
-    year : '2016',
-    getSumary : function(){ return `${this.title}, written by ${this.author} in ${this.year}`;}
-};
+// const book2 = {
+//     title: 'book 2',
+//     author : 'Jane Doe',
+//     year : '2016',
+//     getSumary : function(){ return `${this.title}, written by ${this.author} in ${this.year}`;}
+// };
 
-console.log(book1);
+// console.log(book1);
+// console.log(book1.title);
+// console.log(book1.getSumary());
+// console.log(book2.getSumary());
+// // return array of all values / keys inside:
+// console.log(Object.values(book1));
+// console.log(Object.keys(book1));
+
+// constructors:
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getSumary = function(){ return `${this.title}, written by ${this.author} in ${this.year}`;};
+}
+// instantiate an obj from a ctor
+const book1 = new Book('book 1', 'John Doe', '2013');
+const book2 = new Book('book 2','Jane Doe', '2016' );
+console.log(book1);  // Object {} stuff
 console.log(book1.title);
-console.log(book1.getSumary());
 console.log(book2.getSumary());
-// return array of all values / keys inside:
-console.log(Object.values(book1));
-console.log(Object.keys(book1));
