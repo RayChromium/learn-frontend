792f9bba74ad8154a94fc31a13e5a0489263e93f
RayChromium <raychromium@outlook.com>
2022-11-30 22:58:19 +0800

    Promise.all

--- javascript/aync_crash_course/promises.js
+++ javascript/aync_crash_course/promises.js
@@ -35,3 +35,15 @@ function creatPost(post) {
 
 // creatPost( { title : 'Post Three' , body : 'This is Post Three' } ).then(()=>console.log('promise action'));
 creatPost( { title : 'Post Three' , body : 'This is Post Three' } ).then(getPosts).catch(err=>console.log(err));
+
+// Promise.all
+// .then() after a Promise.all([] : list of promises) will register a callback when all promises in the list resolves
+const promise1 = Promise.resolve('hello!'); // create a resolved promise
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject)=>{
+    setTimeout(resolve, 2000, 'Goodbye');
+})
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then( res => res.json() );
+
+Promise.all([promise1, promise2, promise3, promise4])
+.then(values => console.log(values));
