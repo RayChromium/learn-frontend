19a7e6e5bab30ba288c48e4beaf20b22c75f0f89
RayChromium <raychromium@outlook.com>
2022-12-14 21:15:16 +0800

    rendering the page

--- playground/custom_pdf_viewer/js/main.js
+++ playground/custom_pdf_viewer/js/main.js
@@ -11,11 +11,39 @@ const scale = 1.5,
 
 // Render the page
 const renderPage = num => {
+    pageIsRendering = true;
 
+    // Get the page
+    pdfDoc.getPage(num).then(page => {
+        // Set scale
+        const viewport = page.getViewport({scale:scale});
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+
+        const renderCtx = {
+            canvasContext : ctx, 
+            viewport
+        }
+        page.render(renderCtx).promise.then(()=>{
+            pageIsRendering = false;
+
+            if(pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        });
+
+        // Output current page
+        document.querySelector('#page-num').textContent = num;
+    });
 }
 
 // Get the document
 pdfjsLib.getDocument(url).promise.then( pdfDoc_ => {
     pdfDoc = pdfDoc_;
-    console.log(pdfDoc);
+
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
 } );
