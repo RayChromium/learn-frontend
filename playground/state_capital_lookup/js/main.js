877df44e9e99ce6239aa88e195f3e5154f401393
RayChromium <raychromium@outlook.com>
2022-12-22 20:51:27 +0800

    filter regex

--- playground/state_capital_lookup/js/main.js
+++ playground/state_capital_lookup/js/main.js
@@ -6,7 +6,17 @@ const searchStates = async searchText => {
     const res = await fetch('./data/state_capitals.json');
     const states = await res.json();
 
-    console.log(states);
+    // Get matches to current text input: 
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+
+    if(searchStates.length === 0){
+        matches = [];
+    }
+
+    console.log(matches);
 };
 
 search.addEventListener('input', () => searchStates(search.value));
