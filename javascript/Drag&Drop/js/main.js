dcecba1f328349d0b05f37681e31823810100d0b
RayChromium <raychromium@outlook.com>
2022-12-27 16:31:13 +0800

    more drag events

--- javascript/Drag&Drop/js/main.js
+++ javascript/Drag&Drop/js/main.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Loop through empties and call drag event
+for( const empty of empties ) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
 // Drag functions
 function dragStart() {
     this.className += ' hold';
@@ -14,4 +22,26 @@ function dragStart() {
 
 function dragEnd() {
     this.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    // console.log('over');
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hover';
+    // console.log('enter');
+}
+
+function dragLeave() {
+    // console.log('leave');
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    // console.log('drop');
+    this.className = 'empty';
+    this.append(fill);
 }
