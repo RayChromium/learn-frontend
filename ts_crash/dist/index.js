7e231687f8b20eb9a5389aa664ca705f020f20f6
RayChromium <raychromium@outlook.com>
2023-08-18 16:08:01 +0300

    typescript basic types

--- ts_crash/dist/index.js
+++ ts_crash/dist/index.js
@@ -1,4 +1,21 @@
 "use strict";
 console.log('hello');
+// Basic types:
 let id = 5;
-console.log(`id: ${id}`);
+let company = 'WanderingRay';
+let isPublished = false;
+let x = 'hello'; // any:
+x = true;
+let ids = [1, 2, 3, 4, 5]; // array that contain numbers
+// ids.push('hello'); //error : not asignable
+let arr = [1, true, 'hello'];
+// Tuple
+let person = [1, 'Ray', true];
+// let person2: [number, string, boolean] = [1, 'Ray', 2]; // error: 2 not assinable to boolean
+// Tuple array
+let employee;
+employee = [
+    [1, 'Ray'],
+    [2, 'Laoshu'],
+    [3, 'booboo']
+];
--- ts_crash/src/index.ts
+++ ts_crash/src/index.ts
@@ -1,3 +1,24 @@
 console.log('hello');
+
+// Basic types:
 let id: number = 5;
-console.log(`id: ${id}`)
+let company : string = 'WanderingRay';
+let isPublished : boolean = false;
+
+let x : any = 'hello';// any:
+x = true;
+
+let ids: number[] = [1,2,3,4,5] // array that contain numbers
+// ids.push('hello'); //error : not asignable
+let arr: any[] = [1, true, 'hello'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Ray', true];
+// let person2: [number, string, boolean] = [1, 'Ray', 2]; // error: 2 not assinable to boolean
+// Tuple array
+let employee:  [number, string][];
+employee = [ 
+    [1, 'Ray'],
+    [2, 'Laoshu'],
+    [3, 'booboo']
+ ]
