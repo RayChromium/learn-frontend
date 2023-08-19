a776d1f79f2aba20548d0b04cb946c75349981ab
RayChromium <raychromium@outlook.com>
2023-08-19 15:41:12 +0300

    next_crash: newpage always in coresponding folder

---
+++ next-crash-course/app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita libero id, alias deserunt exercitationem dolore. Sit sunt repellat neque blanditiis hic vel obcaecati, dolorem consequatur, molestiae doloremque eos reprehenderit ipsa!</p>
+    </div>
+  )
+}
+
+export default TeamPage
