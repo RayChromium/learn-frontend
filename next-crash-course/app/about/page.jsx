b78e1d1f1f82b7ec6a303adb2dbf301be9c52594
RayChromium <raychromium@outlook.com>
2023-08-19 15:26:03 +0300

    next_crash: Link routing

---
+++ next-crash-course/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About the cheatsheet</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda eligendi quam quae. Excepturi cum neque laboriosam ipsam. Delectus asperiores cum distinctio, odit non perferendis possimus! Adipisci facere consequuntur veritatis?</p>
+    </div>
+  )
+}
+
+export default AboutPage
