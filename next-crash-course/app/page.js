37b56555501095a1ec47bee1da67262584662f6d
RayChromium <raychromium@outlook.com>
2023-08-20 23:36:06 +0300

    next_crash: fetching course data

--- next-crash-course/app/page.js
+++ next-crash-course/app/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Courses from './components/Courses'
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
         <li><Link href='/about'>About</Link></li>
         <li><Link href='/about/team'>Team</Link></li>
       </ul>
+      <Courses />
     </div>
   )
 }
