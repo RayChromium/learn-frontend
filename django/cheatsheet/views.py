61f7a5293c29234ed8e900528d6aaa4fc8b02c79
RayChromium <raychromium@outlook.com>
2023-11-07 21:28:26 +0200

    django_crash: return complex stuff in html

--- django/cheatsheet/views.py
+++ django/cheatsheet/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+    'cheatsheet': [
+        'wtf is model', 
+        'wtf is view',
+        {
+            'id': 5,
+            'name': 'wert',
+            'time': 2023
+        },
+        {
+            'id': 1,
+            'name': 'qwer',
+            'time': 2012
+        },
+        {
+            'id': 4,
+            'name': 'asdf',
+            'time': 1999
+        }
+        ]
+    }
+
 def cheatsheet(request):
-    return render(request, 'cheatsheet/cheatsheet.html', {'cheatsheet': ['wtf is model', 'wtf is view']})
+    return render(request, 'cheatsheet/cheatsheet.html', data )
 
 def home(request):
     return HttpResponse("Homepage")
